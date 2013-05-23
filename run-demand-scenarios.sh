685f7edadf7f7cafa2cfc27f8dec8ceefe67f63e
Ben Elliston <b.elliston@student.unsw.edu.au>
2013-05-23 20:22:39 +1000

    Add demand scenario #19.

--- run-demand-scenarios.sh
+++ run-demand-scenarios.sh
@@ -47,6 +47,9 @@ scenario[17]="${scenario[11]} -d npeaks:100:-5"
 # scenario 11, plus a 5% demand reduction
 scenario[18]="${scenario[11]} -d scale:-5"
 
+# move morning and evening peaks around noon (3GW in each hour)
+scenario[19]="-d shift:3000:7:10 shift:3000:8:12 shift:3000:9:11 shift:3000:17:12 shift:3000:18:13 shift:3000:19:14" 
+
 for n in `seq 0 18` ; do
     echo "demand scenario $n"
     $PYTHON $EVOLVE $EVOPTS -s re100 ${scenario[$n]}
