966bbabb0e5aa184b38f96a201396917970c436e
Ben Elliston <b.elliston@student.unsw.edu.au>
2013-05-30 21:00:53 +1000

    Update copyright years.

--- costs.py
+++ costs.py
@@ -1,5 +1,5 @@
 # -*- Python -*-
-# Copyright (C) 2012 Ben Elliston
+# Copyright (C) 2012, 2013 Ben Elliston
 #
 # This file is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
