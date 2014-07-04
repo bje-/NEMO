b3fbc61b978166558a06d7bf6a88eba18b946cc9
Ben Elliston <bje@air.net.au>
2014-07-04 11:00:04 +1000

    * costs.py: Move 'escalation' variable up to become a class member.

--- costs.py
+++ costs.py
@@ -24,6 +24,7 @@ class AETA2012_2030:
     """
 
     lifetime = 30
+    escalation = 1.171
 
     def __init__(self, discount, coal_price, gas_price, ccs_price):
         """Construct a cost object given discount rate, coal, gas and CCS costs."""
@@ -36,8 +37,6 @@ class AETA2012_2030:
         self.fixed_om_costs = {}
         self.annuityf = annuity_factor(AETA2012_2030.lifetime, discount)
 
-        escalation = 1.171
-
         # Common capital costs
         table = self.capcost_per_kw_per_yr
         table[tech.Hydro] = 0
@@ -48,15 +47,15 @@ class AETA2012_2030:
         table = self.opcost_per_mwh
         table[tech.Hydro] = 0
         table[tech.PumpedHydro] = 0
-        table[tech.Wind] = 12 * escalation
-        table[tech.CST] = 20 * escalation
+        table[tech.Wind] = 12 * self.escalation
+        table[tech.CST] = 20 * self.escalation
         table[tech.PV] = 0
-        table[tech.Biofuel] = 10 * escalation + 80  # (fuel)
-        table[tech.CCGT] = 4 * escalation
-        table[tech.OCGT] = 10 * escalation
-        table[tech.CCGT_CCS] = 9 * escalation
-        table[tech.Coal_CCS] = 15 * escalation
-        table[tech.Black_Coal] = 7 * escalation
+        table[tech.Biofuel] = 10 * self.escalation + 80  # (fuel)
+        table[tech.CCGT] = 4 * self.escalation
+        table[tech.OCGT] = 10 * self.escalation
+        table[tech.CCGT_CCS] = 9 * self.escalation
+        table[tech.Coal_CCS] = 15 * self.escalation
+        table[tech.Black_Coal] = 7 * self.escalation
         table[tech.Geothermal] = 0
 
         # Fixed O&M (FOM) costs
@@ -64,16 +63,16 @@ class AETA2012_2030:
         table[tech.DemandResponse] = 0
         table[tech.Hydro] = 0
         table[tech.PumpedHydro] = 0
-        table[tech.Wind] = 40 * escalation
-        table[tech.CST] = 65 * escalation
-        table[tech.PV] = 25 * escalation
-        table[tech.Biofuel] = 4 * escalation
-        table[tech.CCGT] = 10 * escalation
-        table[tech.OCGT] = 4 * escalation
-        table[tech.CCGT_CCS] = 17 * escalation
-        table[tech.Coal_CCS] = 73.2 * escalation
-        table[tech.Black_Coal] = 50.5 * escalation
-        table[tech.Geothermal] = 200 * escalation
+        table[tech.Wind] = 40 * self.escalation
+        table[tech.CST] = 65 * self.escalation
+        table[tech.PV] = 25 * self.escalation
+        table[tech.Biofuel] = 4 * self.escalation
+        table[tech.CCGT] = 10 * self.escalation
+        table[tech.OCGT] = 4 * self.escalation
+        table[tech.CCGT_CCS] = 17 * self.escalation
+        table[tech.Coal_CCS] = 73.2 * self.escalation
+        table[tech.Black_Coal] = 50.5 * self.escalation
+        table[tech.Geothermal] = 200 * self.escalation
 
 
 class AETA2012_2030Low (AETA2012_2030):
