4585f403c6227f03bb6027da31598107cb6d51cb
Ben Elliston <bje@air.net.au>
2014-07-05 21:57:55 +0930

    * profile.py: Reduce to being a profiling stub again. Coverage test are   now handled by tests/coverage-test.py.

--- profile.py
+++ profile.py
@@ -1,10 +1,5 @@
 """A stub for profiling tools to run one basic simulation."""
 
-import os
 import nem
 c = nem.Context()
-c.track_exchanges = True
 nem.run(c)
-nem.plot(c, filename='foo.png')
-nem.plot(c, filename='foo.png', spills=True)
-os.unlink('foo.png')
