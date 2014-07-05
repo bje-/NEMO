4049a60df7d38b8811e7303c65fe51bb33111685
Ben Elliston <bje@air.net.au>
2014-07-05 17:27:33 +0930

    * nem.py (_sim): Round gen and residual_hour_demand to two places.

--- nem.py
+++ nem.py
@@ -224,7 +224,8 @@ def _sim(context, starthour, endhour):
         # Dispatch power from each generator in merit order
         for gidx, g in enumerate(gens):
             gen, context.spill[gidx, hr] = g.step(hr, residual_hour_demand)
-            assert gen <= residual_hour_demand, "generation (%.2f) > demand (%.2f) for %s" % (gen, residual_hour_demand, g)
+            assert round(gen, 2) <= round(residual_hour_demand, 2), \
+                "generation (%.2f) > demand (%.2f) for %s" % (gen, residual_hour_demand, g)
             context.generation[gidx, hr] = gen
             if not gen:
                 continue
