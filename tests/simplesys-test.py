7c3b849686c3f47b15b64b14ef298f5e4d4dc749
Ben Elliston <b.elliston@unsw.edu.au>
2014-07-14 13:10:57 +1000

    * tests/simplesys-test.py: Overhaul test case.

--- tests/simplesys-test.py
+++ tests/simplesys-test.py
@@ -32,6 +32,19 @@ class TestSIMPLESYS(unittest.TestCase):
 
     """Tests for SIMPLESYS."""
 
+    def check_row(self, i, row):
+        print [i] + row
+        qc, qa, qs, es, qd, ql, mode = row
+        c = self.context
+        self.context.nexthour(150)
+        self.assertEqual(c.HR - 1, i)
+        self.assertEqual(round(c.QC), round(qc))
+        self.assertEqual(round(c.QA), round(qa))
+        self.assertEqual(round(c.QS), round(qs))
+        self.assertEqual(round(c.ES), round(es))
+        self.assertEqual(round(c.QD), round(qd))
+        self.assertEqual(c.MODE, mode)
+
     def testResultsAgainstTableData(self):
         """Check results match the output given on the SIMPLESYS web page."""
 
@@ -87,13 +100,37 @@ class TestSIMPLESYS(unittest.TestCase):
                 [0, 150, 0, 0, 0, 150, 1]]
 
         for i, row in enumerate(data):
-            qc, qa, qs, es, qd, ql, mode = row
-            c = self.context
-            self.context.nexthour(150)
-            self.assertEqual(c.HR - 1, i)
-            self.assertEqual(round(c.QC), round(qc))
-            self.assertEqual(round(c.QA), round(qa))
-            self.assertEqual(round(c.QS), round(qs))
-            self.assertEqual(round(c.ES), round(es))
-            self.assertEqual(round(c.QD), round(qd))
-            self.assertEqual(c.MODE, mode)
+            self.check_row(i, row)
+
+    def testResultsAgainstTableData_7am_to_3pm(self):
+        """Check results match for a day of limited operation (7am to 3pm)."""
+        self.context = OrigContext(ep=200, qf=10, sl=10, sm=500)
+        self.context.ISTART = 7
+        self.context.SHUT = 15
+        data = [[0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 150, 0, 0, 0, 150, 1],
+                [159, 0, 9, 9, 0, 150, 3],
+                [344, 0, 194, 193, 0, 150, 3],
+                [423, 0, 273, 456, 0, 150, 3],
+                [473, 0, 54, 500, 269, 150, 3.4],
+                [490, 0, 10, 500, 330, 150, 3.4],
+                [473, 0, 10, 500, 313, 150, 3.4],
+                [423, 0, 10, 500, 263, 150, 3.4],
+                [0, 0, 0, 490, 0, 0, 0],
+                [0, 0, 0, 480, 0, 0, 0],
+                [0, 0, 0, 470, 0, 0, 0],
+                [0, 0, 0, 460, 0, 0, 0],
+                [0, 0, 0, 450, 0, 0, 0],
+                [0, 0, 0, 440, 0, 0, 0],
+                [0, 0, 0, 430, 0, 0, 0],
+                [0, 0, 0, 420, 0, 0, 0],
+                [0, 0, 0, 410, 0, 0, 0]]
+
+        for i, row in enumerate(data):
+            self.check_row(i, row)
