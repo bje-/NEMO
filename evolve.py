da34f03be42061a4fe1a9a1bee3eb90526bba168
Ben Elliston <b.elliston@unsw.edu.au>
2014-07-31 20:44:59 +1000

    * evolve.py: Clean up header comment.

--- evolve.py
+++ evolve.py
@@ -1,7 +1,5 @@
 # Copyright (C) 2012, 2013, 2014 Ben Elliston
 #
-# evolve.py -- evolutionary exploration of the NEM
-#
 # This file is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
