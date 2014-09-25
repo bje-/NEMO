7818d0b4fad90280f250c534072fcc9d948d3435
Ben Elliston <b.elliston@unsw.edu.au>
2014-09-26 09:46:50 +1000

    Update copyright notices.

--- nem.py
+++ nem.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2011, 2012, 2014 Ben Elliston
+# Copyright (C) 2014 The University of New South Wales
 #
 # This file is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
