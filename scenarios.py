31224eb20614a562e8ede518bc2227e826100075
Ben Elliston <b.elliston@unsw.edu.au>
2015-08-17 14:05:26 +1000

    * scenarios.py (re100): Correct the simulated gravity hydro capacities   for NSW and Victoria. The NSW total incorrectly included the small   Bendeela and Kangroo Valley pumped hydro stations. The Victorian   total omitted the Murray hydro station (1,500 MW), perhaps because   it was misidentified as Tumut 3 in NSW (also 1,500 MW).  The total   hydro capacity is now 1,027 MW larger.  Thanks to Oscar Wilkie for   the bug report.

--- scenarios.py
+++ scenarios.py
@@ -169,8 +169,8 @@ def re100(context):
         elif g == Hydro:
             # Ignore the one small hydro plant in SA.
             result.append(Hydro(regions.tas, 2740, label=regions.tas.id + ' hydro'))
-            result.append(Hydro(regions.nsw, 1160, label=regions.nsw.id + ' hydro'))
-            result.append(Hydro(regions.vic, 960, label=regions.vic.id + ' hydro'))
+            result.append(Hydro(regions.nsw, 910, label=regions.nsw.id + ' hydro'))
+            result.append(Hydro(regions.vic, 2237, label=regions.vic.id + ' hydro'))
         elif g == Biofuel:
             # 24 GW biofuelled gas turbines (fixed)
             # distribute 24GW of biofuelled turbines across chosen regions
