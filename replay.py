7db8c8d31137c7694726a1c3a57e1349aa77d315
Ben Elliston <b.elliston@unsw.edu.au>
2015-12-15 19:13:54 +1100

    * replay.py: Produce an exchanges.csv file when -t is given.

--- replay.py
+++ replay.py
@@ -53,7 +53,10 @@ def run_one(chromosome):
     context.verbose = args.v > 0
     print context
     if args.transmission:
-        print context.exchanges.max(axis=0)
+        x = context.exchanges.max(axis=0)
+        if args.v > 0:
+            print np.array_str(x, precision=1, suppress_small=True)
+        np.savetxt('exchanges.csv', x, fmt='%.1f', delimiter=',')
 
 context = nem.Context()
 context.nsp_limit = args.nsp_limit
