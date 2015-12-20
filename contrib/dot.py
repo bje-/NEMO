ccf2abc4a22188b41f98563c09444139030fa8cf
Ben Elliston <b.elliston@unsw.edu.au>
2015-12-20 22:38:36 +1100

    * contrib/dot.py: Add license preamble.

--- contrib/dot.py
+++ contrib/dot.py
@@ -1,3 +1,10 @@
+# Copyright (C) 2015 The University of New South Wales
+#
+# This file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
 import numpy as np
 import json
 
