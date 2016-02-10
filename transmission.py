34149b3a2f8594a23ff0aae3b742ce819d477759
Ben Elliston <b.elliston@unsw.edu.au>
2016-02-10 11:59:37 +1100

    * transmission.py (Transmission): Eliminate dist_matrix class member.   Use polygons.distances directly.

--- transmission.py
+++ transmission.py
@@ -7,7 +7,7 @@
 
 """Transmission model details."""
 import numpy as np
-import polygons
+from polygons import distances
 from costs import annuity_factor
 
 
@@ -21,21 +21,19 @@ class Transmission:
         >>> t = Transmission(0.05, 30)
         """
         # Vectorise the cost function so that we can call it with a matrix argument.
-        self.costfn = np.vectorize(costfn)
+        self.cost_per_mw_km = np.vectorize(costfn)
         self.af = annuity_factor(lifetime, discount)
-        self.dist_matrix = polygons.distances
 
     def cost_matrix(self, capacities):
         """Return the cost matrix given a lambda function and capacity matrix.
 
-        >>> t = Transmission(lambda x: x / 10, 30, 0.05)
-        >>> caps = np.empty(polygons.distances.shape)
+        >>> t = Transmission(lambda x: 800, 0.05, 30)
+        >>> caps = np.empty(distances.shape)
         >>> caps.fill(100)
         >>> costmat = t.cost_matrix(caps)
         >>> costmat[1:, 1:].min()
         0.0
         >>> costmat[1:, 1:].max().round()
-        553345356.0
+        15143974.0
         """
-        cost_per_mw_km = self.costfn(capacities)
-        return cost_per_mw_km * capacities * self.dist_matrix / self.af
+        return self.cost_per_mw_km(capacities) * capacities * distances / self.af
