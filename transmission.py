83c2e6d841b36403d128a56835bba0ad59c9aeef
Ben Elliston <b.elliston@unsw.edu.au>
2016-02-10 11:46:46 +1100

    This reverts commit cc6749bc994cd81f0107608a9c585e22524b68de.

--- transmission.py
+++ transmission.py
@@ -7,7 +7,7 @@
 
 """Transmission model details."""
 import numpy as np
-from polygons import distances
+import polygons
 from costs import annuity_factor
 
 
@@ -23,19 +23,19 @@ class Transmission:
         # Vectorise the cost function so that we can call it with a matrix argument.
         self.costfn = np.vectorize(costfn)
         self.af = annuity_factor(lifetime, discount)
-        self.dist_matrix = distances
+        self.dist_matrix = polygons.distances
 
     def cost_matrix(self, capacities):
         """Return the cost matrix given a lambda function and capacity matrix.
 
-        >>> t = Transmission(lambda x: x, 0.05, 30)
-        >>> caps = np.empty(distances.shape)
+        >>> t = Transmission(lambda x: x / 10, 30, 0.05)
+        >>> caps = np.empty(polygons.distances.shape)
         >>> caps.fill(100)
         >>> costmat = t.cost_matrix(caps)
         >>> costmat[1:, 1:].min()
         0.0
         >>> costmat[1:, 1:].max().round()
-        18930.0
+        553345356.0
         """
-        cost_per_km = self.costfn(capacities)
-        return cost_per_km * distances / self.af
+        cost_per_mw_km = self.costfn(capacities)
+        return cost_per_mw_km * capacities * self.dist_matrix / self.af
