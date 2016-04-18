e7bdf3cd810fcbf93a07e040a732b21d363fb2ce
Ben Elliston <b.elliston@unsw.edu.au>
2016-04-18 14:46:10 +1000

    This reverts commit ac7dfbf11440b3a529a8b37728c371da602d9fc5.

--- scenarios.py
+++ scenarios.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2012, 2013, 2014 Ben Elliston
 # Copyright (C) 2014, 2015, 2016 The University of New South Wales
-
+#
 # This file is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
