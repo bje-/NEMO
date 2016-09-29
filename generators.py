f23e9557a843fbac90aa7c851b62ebd9bdc79e0d
Ben Elliston <bje@air.net.au>
2016-09-29 17:21:42 +1000

    * generators.py, scenarios.py: Update copyright notice.

--- generators.py
+++ generators.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2011, 2012, 2013, 2014 Ben Elliston
 # Copyright (C) 2014, 2015, 2016 The University of New South Wales
+# Copyright (C) 2016 IT Power (Australia)
 #
 # This file is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
