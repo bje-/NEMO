491d5c7027f94bdc2f0c848eb55643d6afca1efd
Ben Elliston <bje@air.net.au>
2021-11-26 16:23:42 +1100

    * configfile.py: Pylint fix.

--- nemo/configfile.py
+++ nemo/configfile.py
@@ -14,7 +14,7 @@ import os
 def load(filename):
     """Load a configuration file (or files)."""
     result = config.read(filename)
-    assert result != [], f"config file {filename} not found"
+    assert result, f"config file {filename} not found"
     # Verify
     config.get('generation', 'cst-trace')
     config.get('generation', 'egs-geothermal-trace')
